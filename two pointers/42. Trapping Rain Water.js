69cfe94f4aac2d28db9517a23596ac4917a75437
zbl14 <ifthereisoneday@gmail.com>
2022-10-13 12:41:12 -0700

    solved

---
+++ two pointers/42. Trapping Rain Water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let leftMax = 0;
+  let rightMax = 0;
+  let left = 0;
+  let right = height.length - 1;
+  let res = 0;
+  while (left < right) {
+    leftMax = Math.max(leftMax, height[left]);
+    rightMax = Math.max(rightMax, height[right]);
+    if (height[left] < height[right]) {
+      res += leftMax - height[left];
+      left++;
+    } else {
+      res += rightMax - height[right];
+      right--;
+    }
+  }
+  return res;
+};
