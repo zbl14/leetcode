9acdb92e54e4b82d47c3b543ba038ec79821f08d
zbl14 <ifthereisoneday@gmail.com>
2022-10-20 10:59:14 -0700

    solved

---
+++ two pointers/80. Remove Duplicates from Sorted Array II.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length <= 2) return nums.length;
+  let j = 2;
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] !== nums[j - 2] || nums[i] !== nums[j - 1]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+};
