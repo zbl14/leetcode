9e14cee5ae72d560e28df337a7f4e083992f13dd
Zhibin Liang <ifthereisoneday@gmail.com>
2022-12-31 15:31:04 -0500

    add comment

---
+++ divide-and-conquer/215. Kth Largest Element in an Array.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  let l = 0,
+    r = nums.length - 1,
+    target = nums.length - k;
+  // divide
+  while (l < r) {
+    let mid = quickSelection(nums, l, r);
+    if (mid == target) {
+      return nums[mid];
+    }
+    mid < target ? (l = mid + 1) : (r = mid - 1);
+  }
+  return nums[l];
+};
+
+// conquer
+const quickSelection = (nums, l, r) => {
+  let pivot = nums[r];
+  let wall = l;
+  for (let i = l; i < r; i++) {
+    if (nums[i] < pivot) {
+      swap(nums, i, wall);
+      wall++;
+    }
+  }
+  swap(nums, wall, r);
+  return wall;
+};
+
+const swap = (nums, i, j) => {
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+};
