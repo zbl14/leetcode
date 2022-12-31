8ee78b92897b006e7302576ec0b75892845dbee2
Zhibin Liang <ifthereisoneday@gmail.com>
2022-12-31 15:21:28 -0500

    update

--- sorting algorithm/quick select/215. Kth Largest Element in an Array.js
+++ sorting algorithm/quick select/215. Kth Largest Element in an Array.js
@@ -3,6 +3,11 @@
  * @param {number} k
  * @return {number}
  */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 var findKthLargest = function (nums, k) {
   let l = 0,
     r = nums.length - 1,
@@ -19,15 +24,15 @@ var findKthLargest = function (nums, k) {
 
 const quickSelection = (nums, l, r) => {
   let pivot = nums[r];
-  let i = l - 1;
-  for (let j = l; j < r; j++) {
-    if (nums[j] < pivot) {
-      i++;
-      swap(nums, i, j);
+  let wall = l;
+  for (let i = l; i < r; i++) {
+    if (nums[i] < pivot) {
+      swap(nums, i, wall);
+      wall++;
     }
   }
-  swap(nums, i + 1, r);
-  return i + 1;
+  swap(nums, wall, r);
+  return wall;
 };
 
 const swap = (nums, i, j) => {
