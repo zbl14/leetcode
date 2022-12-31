ab681042dea144e4624aec870e0c65a80b5d97fb
Zhibin Liang <ifthereisoneday@gmail.com>
2022-12-30 23:21:33 -0500

    solved

---
+++ divide-and-conquer/169. Majority Element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const len = nums.length;
+  if (len === 1) return nums[0];
+  const numFreq = {};
+  for (let i = 0; i < len; i++) {
+    if (!numFreq[nums[i]]) {
+      numFreq[nums[i]] = 1;
+    } else {
+      numFreq[nums[i]]++;
+      if (numFreq[nums[i]] > len / 2) return nums[i];
+    }
+  }
+};
