095f7814613a6d1133140979604e5e7bb1d5bff6
Zhibin Liang <ifthereisoneday@gmail.com>
2023-01-11 17:22:20 -0500

    update

--- prefix sum/560. Subarray Sum Equals K/560. Subarray Sum Equals K-2.js
+++ prefix sum/560. Subarray Sum Equals K/560. Subarray Sum Equals K-2.js
@@ -3,14 +3,14 @@
  * @param {number} k
  * @return {number}
  */
-//TC = O(n)
-// preSum - preSum = target,所以我们要找的是map里是否存在preSum - target
+//TC = O(n), SC = O(n)
+// new preSum - old preSum = target,所以我们要找的是map里是否存在old preSum
 var subarraySum = function (nums, k) {
   const map = new Map();
   let sum = 0,
     res = 0;
   map.set(0, 1);
-  for (let num of nums) {
+  for (const num of nums) {
     sum += num;
     if (map.has(sum - k)) res += map.get(sum - k);
     map.set(sum, (map.get(sum) || 0) + 1);
