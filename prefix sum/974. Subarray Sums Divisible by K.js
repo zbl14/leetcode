f4e8d23480f22c9f1440303e4fa58023d42f431e
Zhibin Liang <ifthereisoneday@gmail.com>
2023-01-11 23:04:45 -0500

    update

--- prefix sum/974. Subarray Sums Divisible by K.js
+++ prefix sum/974. Subarray Sums Divisible by K.js
@@ -4,11 +4,12 @@
  * @return {number}
  */
 var subarraysDivByK = function (nums, k) {
-  const map = new Map();
+  const map = new Map(); //记录remainder，frequency
   let prefix = 0;
   let res = 0;
   map.set(0, 1);
   for (const num of nums) {
+    // +k是为了避免num为负数的情况，prefix记录的是prefix除以k所的余数
     prefix = (prefix + (num % k) + k) % k;
     res += map.get(prefix) || 0;
     map.set(prefix, (map.get(prefix) || 0) + 1);
