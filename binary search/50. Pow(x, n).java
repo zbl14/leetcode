1111034d0210bb5aab74f14718243b918a100de0
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-14 21:39:13 -0400

    solved

---
+++ binary search/50. Pow(x, n).java
@@ -0,0 +1,9 @@
+// TC = O(logn), SC = O(logn)
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        else if (n % 2 == 1) return x * myPow(x, n - 1);
+        else if (n % 2 == 0) return myPow(x * x, n / 2);
+        else return 1 / myPow(x, -n);
+    }
+}
---
+++ binary search/702. Search in a Sorted Array of Unknown Size/702. Search in a Sorted Array of Unknown Size-2.java
@@ -0,0 +1,27 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int l = 0;
+        int r = target - reader.get(0);
+        /*  Why right = target - reader.get(0) ? 
+		Explanation: Given all the elements in the Array are unique. If first value is n and we have all possible integers from n to target . 
+		We will find the target at worst case  at (target - n) 
+		Example: If First Element is -1 and target = 5 if the array had all elements between -1 and 5 i.e.,  
+		[-1,0,1,2,3,4,5] Worst case 5 will be at (target - firstValue)
+		*/
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(reader.get(mid) == target) return mid;
+            else if(reader.get(mid) < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return -1;
+    }
+}
