e1109669d8436d7b9fdcc05254f8c861a8bde306
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-18 14:55:33 -0400

    solved

---
+++ bfs/127. Word Ladder/Solution.java
@@ -0,0 +1,74 @@
+// class Solution {
+//   public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//     Set<String> dict = new HashSet<>(wordList);
+//     if (!dict.contains(endWord))
+//       return 0;
+
+//     Queue<String> q = new LinkedList<>();
+//     int res = 1;
+//     q.offer(beginWord);
+//     while (!q.isEmpty()) {
+//       int size = q.size();
+//       for (int i = 0; i < size; i++) {
+//         String word = q.poll();
+//         if (word.equals(endWord))
+//           // In Java, == checks whether two references refer to the same object in memory,
+//           // not whether the objects have the same value. To compare strings by value, you
+//           // should use the equals() method instead of the == operator.
+//           return res;
+//         for (int j = 0; j < beginWord.length(); j++) {
+//           for (char letter = 'a'; letter <= 'z'; letter++) {
+//             StringBuilder next = new StringBuilder(word);
+//             next.setCharAt(j, letter);
+//             String nextWord = next.toString();
+//             if (dict.contains(nextWord)) {
+//               if (nextWord.equals(endWord))
+//                 return res + 1;
+//               dict.remove(nextWord);
+//               q.offer(nextWord);
+//             }
+//           }
+//         }
+//       }
+//       res++;
+//     }
+//     return 0;
+//   }
+// }
+
+class Solution {
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    Set<String> dict = new HashSet<>(wordList);
+    if (!dict.contains(endWord))
+      return 0;
+
+    Queue<String> q = new LinkedList<>();
+    int res = 1;
+    q.offer(beginWord);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        String word = q.poll();
+        if (word.equals(endWord))
+          return res;
+        char[] chs = word.toCharArray();
+        for (int j = 0; j < beginWord.length(); j++) {
+          for (char letter = 'a'; letter <= 'z'; letter++) {
+            char pre = chs[j];
+            chs[j] = letter;
+            String nextWord = new String(chs);
+            if (dict.contains(nextWord)) {
+              if (nextWord.equals(endWord))
+                return res + 1;
+              dict.remove(nextWord);
+              q.offer(nextWord);
+            }
+            chs[j] = pre;
+          }
+        }
+      }
+      res++;
+    }
+    return 0;
+  }
+}
---
+++ bfs/199. Binary Tree Right Side View/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+  public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    Queue<TreeNode> q = new LinkedList<>();
+    if (root != null)
+      q.offer(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode cur = q.poll();
+        if (i == size - 1) // 每一层最后一个则为最右边的node，若求left view，i == 0
+          res.add(cur.val);
+        if (cur.left != null)
+          q.offer(cur.left);
+        if (cur.right != null)
+          q.offer(cur.right);
+      }
+    }
+    return res;
+  }
+}
