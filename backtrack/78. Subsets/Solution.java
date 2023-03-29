5666231fe5eada485b888dbe068276fc18c9aedb
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-28 23:08:43 -0400

    updated

--- backtrack/78. Subsets/Solution.java
+++ backtrack/78. Subsets/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
@@ -5,9 +8,9 @@ class Solution {
         return res;
     }
 
-    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, int start){
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, int start) {
         res.add(new ArrayList<>(temp));
-        for(int i = start; i < nums.length; i++){
+        for (int i = start; i < nums.length; i++) {
             temp.add(nums[i]);
             backtrack(res, temp, nums, i + 1);
             temp.remove(temp.size() - 1);
