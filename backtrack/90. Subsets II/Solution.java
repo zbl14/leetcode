5f071fa17a76f6522fcb554e4f96e4de357bedb0
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-28 23:20:04 -0400

    solved

---
+++ backtrack/90. Subsets II/Solution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(res, new ArrayList<>(), nums, 0);
+    return res;
+  }
+
+  private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, int idx) {
+    res.add(new ArrayList<>(temp));
+    for (int i = idx; i < nums.length; i++) {
+      if (i != idx && nums[i] == nums[i - 1])
+        continue;
+      temp.add(nums[i]);
+      backtrack(res, temp, nums, i + 1);
+      temp.remove(temp.size() - 1);
+    }
+  }
+}
