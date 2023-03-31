b4e453c49e557913445042f6b3376e085baed175
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-31 12:58:16 -0400

    solved

---
+++ bfs/111. Minimum Depth of Binary Tree/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+  public int minDepth(TreeNode root) {
+    if (root == null)
+      return 0;
+    int height = 1;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.offer(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      List<Integer> level = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode cur = q.poll();
+        if (cur.left == null && cur.right == null)
+          return height;
+        if (cur.left != null)
+          q.offer(cur.left);
+        if (cur.right != null)
+          q.offer(cur.right);
+      }
+      height++;
+    }
+    return height;
+  }
+}
