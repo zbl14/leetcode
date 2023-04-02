c7a9b324a3dbb4d6f3b9b0a4524357be825c628d
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-02 12:54:00 -0400

    updated

--- 基础数据结构/Hash Table/49. Group Anagrams/Solution.java
+++ 基础数据结构/Hash Table/49. Group Anagrams/Solution.java
@@ -1,6 +1,23 @@
 // TC = O(n * k * log(k)), SC = O(n * k)
 // where n is the length of the input array strs, 
 // and k is the maximum length of a string in strs.
+// class Solution {
+//   public List<List<String>> groupAnagrams(String[] strs) {
+//     if (strs == null)
+//       return Collections.emptyList();
+
+//     Map<String, List<String>> map = new HashMap<>();
+//     for (String str : strs) {
+//       char[] arr = str.toCharArray();
+//       Arrays.sort(arr);
+//       String sortedWord = new String(arr);
+//       map.computeIfAbsent(sortedWord, k -> new ArrayList<>()).add(str);
+//     }
+//     return new ArrayList<>(map.values());
+//   }
+// }
+
+// TC = O(n * k), SC = O(n * k)
 class Solution {
   public List<List<String>> groupAnagrams(String[] strs) {
     if (strs == null)
@@ -8,10 +25,11 @@ class Solution {
 
     Map<String, List<String>> map = new HashMap<>();
     for (String str : strs) {
-      char[] arr = str.toCharArray();
-      Arrays.sort(arr);
-      String sortedWord = new String(arr);
-      map.computeIfAbsent(sortedWord, k -> new ArrayList<>()).add(str);
+      char[] keys = new char[26];
+      for (int i = 0; i < str.length(); i++)
+        keys[str.charAt(i) - 'a']++;
+      String key = new String(keys);
+      map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
     }
     return new ArrayList<>(map.values());
   }
