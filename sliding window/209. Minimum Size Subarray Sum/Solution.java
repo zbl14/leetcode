44d9f08797618dbd6f6432e54d30603b79f30215
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-02 12:08:50 -0400

    solved

---
+++ sliding window/209. Minimum Size Subarray Sum/Solution.java
@@ -0,0 +1,16 @@
+// TC = O(n), SC = O(1)
+class Solution {
+  public int minSubArrayLen(int target, int[] nums) {
+    int l = 0;
+    int sum = 0;
+    int res = Integer.MAX_VALUE;
+    for (int r = 0; r < nums.length; r++) {
+      sum += nums[r];
+      while (sum >= target) {
+        res = Math.min(res, r - l + 1);
+        sum -= nums[l++];
+      }
+    }
+    return res == Integer.MAX_VALUE ? 0 : res;
+  }
+}
