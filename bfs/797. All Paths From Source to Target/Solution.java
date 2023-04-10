ebd8d50a2b7efbc588d1f3d20ee53ac28a3560af
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-10 11:19:29 -0400

    solved

---
+++ bfs/797. All Paths From Source to Target/Solution.java
@@ -0,0 +1,56 @@
+class Solution {
+  public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    List<List<Integer>> paths = new ArrayList<List<Integer>>();
+    Queue<List<Integer>> q = new LinkedList<List<Integer>>();
+
+    // for(int i=0;i<graph[0].length;i++){
+    // List<Integer> path = new ArrayList<Integer>();
+    // path.add(0);
+    // path.add(graph[0][i]);
+    // q.offer(path);
+    // }
+
+    // graph = [[1,2],[3],[3],[]]
+    // q = [[0,1],[0,2]]
+
+    q.offer(Arrays.asList(0));
+    // q = [[0]]
+
+    while (!q.isEmpty()) {
+      List<Integer> list = q.poll();
+      int node = list.get(list.size() - 1);
+      if (node == graph.length - 1) {
+        paths.add(list);
+      }
+
+      for (int i = 0; i < graph[node].length; i++) {
+        List<Integer> newPath = new ArrayList<Integer>(list);
+        newPath.add(graph[node][i]);
+        q.offer(newPath);
+      }
+    }
+    return paths;
+  }
+}
+
+// dfs recursive
+class Solution {
+  public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    List<List<Integer>> paths = new ArrayList<List<Integer>>();
+    backtrack(graph, paths, new ArrayList<>(Arrays.asList(0)), 0);
+    return paths;
+  }
+
+  private void backtrack(int[][] graph, List<List<Integer>> paths, List<Integer> path, int node) {
+    if (node == graph.length - 1) {
+      paths.add(new ArrayList<>(path));
+      return;
+    }
+
+    for (int nextNode : graph[node]) {
+      path.add(nextNode);
+      backtrack(graph, paths, path, nextNode);
+      path.remove(path.size() - 1);
+    }
+  }
+}
---
+++ dfs/841. Keys and Rooms/Solution.java
@@ -0,0 +1,64 @@
+// dfs recursive
+class Solution {
+  private Set<Integer> visited = new HashSet<>();
+
+  public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+    visited.add(0);
+    dfs(rooms, 0);
+    return visited.size() == rooms.size();
+  }
+
+  private void dfs(List<List<Integer>> rooms, int node) {
+    for (int nextNode : rooms.get(node)) {
+      if (!visited.contains(nextNode)) {
+        visited.add(nextNode);
+        dfs(rooms, nextNode);
+      }
+    }
+  }
+}
+
+// bfs
+// class Solution {
+// public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+// Set<Integer> visited = new HashSet<>();
+// Queue<Integer> q = new LinkedList<>();
+// q.offer(0);
+// visited.add(0);
+// int cnt = 0;
+// while(!q.isEmpty()){
+// int node = q.poll();
+// for(int nextNode : rooms.get(node)){
+// if(!visited.contains(nextNode)){
+// q.offer(nextNode);
+// visited.add(nextNode);
+// }
+// }
+// cnt++;
+// }
+// return cnt == rooms.size();
+// }
+// }
+
+// dfs with stack
+// class Solution {
+// public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+// Set<Integer> visited = new HashSet<>();
+// Stack<Integer> stack = new Stack<>();
+// stack.push(0);
+// visited.add(0);
+// int cnt = 0;
+// while(!stack.isEmpty()){
+// int node = stack.pop();
+
+// for(int nextNode : rooms.get(node)){
+// if(!visited.contains(nextNode)){
+// stack.push(nextNode);
+// visited.add(nextNode);
+// }
+// }
+// cnt++;
+// }
+// return cnt == rooms.size();
+// }
+// }
