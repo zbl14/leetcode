bb61c6f9981544cb7b24ea0bb5aa017f25f25648
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-20 22:59:45 -0400

    updated

--- sorting algorithm/quick select/215. Kth Largest Element in an Array/Solution.java
+++ sorting algorithm/quick select/215. Kth Largest Element in an Array/Solution.java
@@ -1,5 +1,18 @@
 import java.util.Random;
 
+// TC = O(nlogk), SC = O(k)
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+//         for(int num : nums){
+//             minHeap.add(num);
+//             if(minHeap.size() > k) minHeap.poll();
+//         }
+//         return minHeap.poll();
+//     }
+// }
+
+// TC = O(n), SC = O(1)
 class Solution {
   public int findKthLargest(int[] nums, int k) {
     shuffle(nums);
