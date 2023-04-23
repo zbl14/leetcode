e6dcbdac025691fa94f8a338f2111b5221511e37
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-23 13:00:50 -0400

    solved

---
+++ binary search/81. Search in Rotated Sorted Array II/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+  public boolean search(int[] nums, int target) {
+    if (nums == null || nums.length == 0)
+      return false;
+    int l = 0;
+    int r = nums.length - 1;
+    while (l <= r) {
+      while (l < r && nums[l] == nums[l + 1])
+        l++;
+      while (l < r && nums[r] == nums[r - 1])
+        r--;
+      int mid = l + (r - l) / 2;
+      if (nums[mid] == target)
+        return true;
+      else if (nums[mid] >= nums[l]) {
+        if (nums[l] <= target && target < nums[mid])
+          r = mid - 1;
+        else
+          l = mid + 1;
+      } else {
+        if (nums[mid] < target && target <= nums[r])
+          l = mid + 1;
+        else
+          r = mid - 1;
+      }
+    }
+    return false;
+  }
+}
