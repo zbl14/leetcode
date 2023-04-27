e631d1a5526f205cb904c2264899388a979f649a
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-27 13:50:48 -0400

    solved

---
+++ divide-and-conquer/114. Flatten Binary Tree to Linked List/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+  public void flatten(TreeNode root) {
+    flattenTree(root);
+  }
+
+  private TreeNode flattenTree(TreeNode node) {
+    if (node == null)
+      return null;
+    if (node.left == null && node.right == null)
+      return node;
+    TreeNode left = flattenTree(node.left);
+    TreeNode right = flattenTree(node.right);
+    if (left != null) {
+      while (left.right != null) {
+        left = left.right;
+      }
+      left.right = node.right;
+      node.right = node.left;
+      node.left = null;
+    }
+    return right == null ? left : right;
+  }
+}
