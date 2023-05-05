d2c0093671c0b896a2ee532ae0c05597992084bd
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-05 13:10:04 -0400

    solved

---
+++ 基础数据结构/Linked List/445. Add Two Numbers II/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    l1 = reverse(l1);
+    l2 = reverse(l2);
+
+    ListNode dummy = new ListNode();
+    ListNode cur = dummy;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int sum = carry;
+      if (l1 != null) {
+        sum += l1.val;
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        sum += l2.val;
+        l2 = l2.next;
+      }
+      cur.next = new ListNode(sum % 10);
+      cur = cur.next;
+      carry = sum / 10;
+    }
+    if (carry > 0)
+      cur.next = new ListNode(carry);
+    return reverse(dummy.next);
+  }
+
+  private ListNode reverse(ListNode list) {
+    ListNode prev = null;
+    ListNode cur = list;
+    while (cur != null) {
+      ListNode next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    return prev;
+  }
+}
