e26e9436a72610f9c0d8c9bf435fda816c28fc8e
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-09 16:04:15 -0400

    solved

---
+++ 基础数据结构/Hash Table/692. Top K Frequent Words/Solution.java
@@ -0,0 +1,28 @@
+// minHeap, TC = O(nlogk)
+class Solution {
+  public List<String> topKFrequent(String[] words, int k) {
+    Map<String, Integer> map = new HashMap<>();
+    for (String word : words) {
+      map.put(word, map.getOrDefault(word, 0) + 1);
+    }
+
+    PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+      if (map.get(a) == map.get(b)) {
+        return b.compareTo(a);
+      } else {
+        return map.get(a) - map.get(b);
+      }
+    });
+    for (String word : map.keySet()) {
+      pq.add(word);
+      if (pq.size() > k) {
+        pq.poll();
+      }
+    }
+
+    List<String> res = new LinkedList<>();
+    while (!pq.isEmpty())
+      res.add(0, pq.poll());
+    return res;
+  }
+}
