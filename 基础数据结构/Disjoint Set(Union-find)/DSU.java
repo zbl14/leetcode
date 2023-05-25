c4b9c3e853f2927de21b5c9536fd211343158cef
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-25 14:30:39 -0400

    update DSU by size

--- 基础数据结构/Disjoint Set(Union-find)/DSU.java
+++ 基础数据结构/Disjoint Set(Union-find)/DSU.java
@@ -56,3 +56,39 @@ class DSU {
     }
   }
 }
+
+class DSU {
+  int[] parent;
+  int[] size;
+
+  public DSU(int n) {
+    parent = new int[n];
+    size = new int[n];
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
+      size[i] = 1;
+    }
+  }
+
+  public int find(int x) {
+    if (parent[x] != x)
+      parent[x] = find(parent[x]);
+    return parent[x];
+  }
+
+  public void union(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX == rootY)
+      return;
+
+    if (size[rootX] < size[rootY]) {
+      int tmp = rootX;
+      rootX = rootY;
+      rootY = tmp;
+    }
+    parent[rootY] = rootX;
+    size[rootX] += size[rootY];
+  }
+}
