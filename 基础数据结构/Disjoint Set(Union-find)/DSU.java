a56868908f279a8462338279f52db94b85770d75
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-25 11:57:00 -0400

    update DSU by rank

--- 基础数据结构/Disjoint Set(Union-find)/DSU.java
+++ 基础数据结构/Disjoint Set(Union-find)/DSU.java
@@ -1,14 +1,58 @@
-class DSU{
+class DSU {
   int[] parent;
-  public DSU(int N){
+
+  public DSU(int N) {
     parent = new int[N];
-    for(int i = 0; i< N; i++) parent[i] = i;
+    for (int i = 0; i < N; i++)
+      parent[i] = i;
   }
-  public int find(int x){
-    if(parent[x] !=x) parent[x] = find(parent[x]);
+
+  public int find(int x) {
+    if (parent[x] != x)
+      parent[x] = find(parent[x]);
     return parent[x];
   }
-  public void union(int x, int y){
+
+  public void union(int x, int y) {
     parent[find(x)] = find(y);
+  }
+}
+
+class DSU {
+  private int[] parent;
+  private int[] rank;
+
+  public DSU(int n) {
+    parent = new int[n];
+    rank = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
+    }
+  }
+
+  public int find(int x) {
+    if (parent[x] != x) {
+      parent[x] = find(parent[x]);
+    }
+    return parent[x];
+  }
+
+  public void union(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX == rootY) {
+      return;
+    }
+
+    if (rank[rootX] < rank[rootY]) {
+      parent[rootX] = rootY;
+    } else if (rank[rootX] > rank[rootY]) {
+      parent[rootY] = rootX;
+    } else {
+      parent[rootY] = rootX;
+      rank[rootX]++;
+    }
   }
 }
