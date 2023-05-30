744e545052e20001dff0b582dbfd93024063d58c
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-30 11:37:36 -0400

    updated

--- 基础数据结构/Graph/277. Find the Celebrity/Solution.java
+++ 基础数据结构/Graph/277. Find the Celebrity/Solution.java
@@ -1,12 +1,41 @@
 /* The knows API is defined in the parent class Relation.
       boolean knows(int a, int b); */
+// brute force, TC = O(n^2), SC = O(1), assuming TC and SC of knows API are both O(1)
+// public class Solution extends Relation {
+//   public int findCelebrity(int n) {
+//     for (int i = 0; i < n; i++) {
+//       if (isCelebrity(i, n)) {
+//         return i;
+//       }
+//     }
+//     return -1;
+//   }
+
+//   private boolean isCelebrity(int i, int n) {
+//     for (int j = 0; j < n; j++) {
+//       if (i == j)
+//         continue;
+//       if (knows(i, j) || !knows(j, i)) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
 
 public class Solution extends Relation {
   public int findCelebrity(int n) {
+    int candidate = 0;
     for (int i = 0; i < n; i++) {
-      if (isCelebrity(i, n)) {
-        return i;
+      if (knows(candidate, i)) {
+        candidate = i;
       }
+    }
+    if (isCelebrity(candidate, n)) {
+      return candidate;
     }
     return -1;
   }
