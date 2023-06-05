bb0c5278728a7002296a0e8948e7bcec7a6c1424
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-04 21:25:47 -0400

    updated

--- 基础数据结构/Disjoint Set(Union-find)/547. Number of Provinces/Solution.java
+++ 基础数据结构/Disjoint Set(Union-find)/547. Number of Provinces/Solution.java
@@ -1,80 +1,105 @@
-// dfs
+// Union find
 // class Solution {
-//   public int findCircleNum(int[][] isConnected) {
-//       int res = 0;
-//       boolean[] visited = new boolean[isConnected.length];
-
-//       for(int i = 0; i < isConnected.length; i++){
-//           if(!visited[i]){
-//               res++;
-//               dfs(i, isConnected, visited);
-//           }
-//       }
-//       return res;
-//   }
-
-//   private void dfs(int node, int[][] isConnected, boolean[] visited){
-//       visited[node] = true;
-//       for(int i = 0; i < isConnected.length; i++){
-//           if(isConnected[node][i] == 1 && !visited[i]){
-//               dfs(i, isConnected, visited);
-//           }
-//       }
-//   }
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         DSU dsu = new DSU(n);
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(isConnected[i][j] == 1) dsu.union(i, j);
+//             }
+//         }
+//         int res = 0;
+//         for(int i = 0; i < n; i++) {
+//             if(dsu.find(i) == i) res++;
+//         }
+//         return res;
+//     }
 // }
 
+// class DSU {
+//     int parent[];
+//     int rank[];
+
+//     public DSU(int n){
+//         parent = new int[n];
+//         rank = new int[n];
+
+//         for(int i = 0; i < n; i++){
+//             parent[i] = i;
+//         }
+//     }
+
+//     public int find(int x){
+//         if(parent[x] != x) parent[x] = find(parent[x]);
+//         return parent[x];
+//     }
+
+//     public void union(int x, int y){
+//         int rootX = find(x);
+//         int rootY = find(y);
+
+//         if(rootX == rootY) return;
+
+//         if(rank[rootX] < rank[rootY]){
+//             parent[rootX] = rootY; 
+//         } else if(rank[rootX] > rank[rootY]){
+//             parent[rootY] = rootX;
+//         } else {
+//             parent[rootY] = rootX;
+//             rank[rootX]++;
+//         }
+//     }
+// }
+
+// dfs
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int res = 0;
+//         boolean[] visited = new boolean[isConnected.length];
+
+//         for(int i = 0; i < isConnected.length; i++){
+//             if(!visited[i]){
+//                 res++;
+//                 dfs(i, isConnected, visited);
+//             }
+//         }
+//         return res;
+//     }
+
+//     private void dfs(int node, int[][] isConnected, boolean[] visited){
+//         visited[node] = true;
+//         for(int i = 0; i < isConnected.length; i++){
+//             if(isConnected[node][i] == 1 && !visited[i]){
+//                 dfs(i, isConnected, visited);
+//             }
+//         }
+//     }
+// }
+
+// bfs
 class Solution {
   public int findCircleNum(int[][] isConnected) {
-    int n = isConnected.length;
-    DSU dsu = new DSU(n);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (isConnected[i][j] == 1)
-          dsu.union(i, j);
+    int res = 0;
+    boolean[] visited = new boolean[isConnected.length];
+
+    for (int i = 0; i < isConnected.length; i++) {
+      if (!visited[i]) {
+        res++;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(i);
+        visited[i] = true;
+
+        while (!q.isEmpty()) {
+          int cur = q.poll();
+          for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[cur][j] == 1 && !visited[j]) {
+              q.offer(j);
+              visited[j] = true;
+            }
+          }
+        }
       }
     }
-    int res = 0;
-    for (int i = 0; i < n; i++) {
-      if (dsu.find(i) == i)
-        res++;
-    }
     return res;
-  }
-}
-
-class DSU {
-  int parent[];
-  int rank[];
-
-  public DSU(int n) {
-    parent = new int[n];
-    rank = new int[n];
-
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-    }
-  }
-
-  public int find(int x) {
-    if (parent[x] != x)
-      parent[x] = find(parent[x]);
-    return parent[x];
-  }
-
-  public void union(int x, int y) {
-    int rootX = find(x);
-    int rootY = find(y);
-
-    if (rootX == rootY)
-      return;
-
-    if (rank[rootX] < rank[rootY]) {
-      parent[rootX] = rootY;
-    } else if (rank[rootX] > rank[rootY]) {
-      parent[rootY] = rootX;
-    } else {
-      parent[rootY] = rootX;
-      rank[rootX]++;
-    }
   }
 }
